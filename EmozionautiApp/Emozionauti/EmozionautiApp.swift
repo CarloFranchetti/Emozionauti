4376dbf7d8a55d96fa0780a2157f15ca061a3224
CarloFranchetti <c.franchetti@studenti.unisa.it>
2025-07-22 15:38:54 +0200

    fix accesso admin

--- EmozionautiApp/Emozionauti/EmozionautiApp.swift
+++ EmozionautiApp/Emozionauti/EmozionautiApp.swift
@@ -4,7 +4,7 @@ import SwiftUI
 struct EmozionautiApp: App {
     @StateObject var navManager = NavigationManager()
     @StateObject var diaryViewModel = DiaryViewModel()
-
+    
     var body: some Scene {
         WindowGroup {
             ContentView(diaryViewModel: diaryViewModel)
